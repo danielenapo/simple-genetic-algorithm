f6b5e5075e91be389b76a58f68e9412c51ef6023
Daniele Napolitano <33985608+danielenapo@users.noreply.github.com>
2018-05-28 15:27:51 +0200

    working algorithm

--- algorithm.js
+++ algorithm.js
@@ -6,31 +6,30 @@ function Individual(){
   this.chromosome=[];
 
   this.generateChromosome= function(){
-    for(var i=0; i<this.numeroGeni; i++){
+    for(var a=0; a<this.numeroGeni; a++){
       this.chromosome.push(Math.round(Math.random()));//assegna valore random tra 0 e 1
     }
   }
 
   this.calculateFitness= function(){
     this.fitness=0;
-    for(var i=0; i<this.chromosome.length; i++){
-      if (this.chromosome[i]==1)
+    for(var b=0; b<this.chromosome.length; b++){
+      if (this.chromosome[b]==1)
         this.fitness++;
     }
   }
 
   this.mutation= function(){
     if(Math.round(Math.random()*100)>=93){
-      alert("c'e' una mutazione!!!");
-      for(var i=2; i<5; i++){
-        if(this.chromosome[i]==0)
-          this.chromosome[i]=1;
+    //  alert("c'e' una mutazione!!!");
+      for(var c=2; c<5; c++){
+        if(this.chromosome[c]==0)
+          this.chromosome[c]=1;
         else
-          this.chromosome[i]=0;
+          this.chromosome[c]=0;
       }
     }
   }
-
 //FINE OGGETTO
 }
 
@@ -39,53 +38,59 @@ function Individual(){
 function main(){
 
   population=[];
+  matingPool=[];
+  populationSize=15;
   //riempi popolazione + inizializzazioni
-  for(var i=0; i<5; i++){
+  for(var d=0; d<populationSize; d++){
     individuo= new Individual();
     individuo.generateChromosome();
     individuo.calculateFitness();
     population.push(individuo);
   }
   bestFitted=new Individual(); bestFitted.fitness=0;
-  secondBestFitted= new Individual(); secondBestFitted.fitness=0;
   generations=0;
   isMutato=0;
   calculateBestFitted();
-  calculateSecondBestFitted();
   //INIZIO SIMULAZIONE
   do{
+    selection();
     generateSon();
     killTheWeakest();
     killTheWeakest();
     calculateBestFitted();
-    calculateSecondBestFitted();
     generations++;
     message="generation "+generations;
     document.getElementById("numero").innerHTML=message;
     message="fitness: "+bestFitted.fitness;
     document.getElementById("fittest").innerHTML=message;
-  }while(bestFitted.fitness!=8);
+    if(generations>300){
+      alert("convergenza prematura");
+      return;
+    }
 
+  }while(bestFitted.fitness!=bestFitted.chromosome.length);
   alert("soluzione trovata alla generazione "+generations);
 }
 
-
 function calculateBestFitted(){
-  for(var i=0; i<population.length; i++){
-    population[i].calculateFitness();
-    if(population[i].fitness>bestFitted.fitness)
-      bestFitted=population[i];
+  for(var e=0; e<population.length; e++){
+    population[e].calculateFitness();
+    if(population[e].fitness>bestFitted.fitness)
+      bestFitted=population[e];
   }
 }
 
-function calculateSecondBestFitted(){
-  for(var i=0; i<population.length; i++){
-    if(population[i].fitness<bestFitted.fitness && population[i].fitness>secondBestFitted.fitness)
-      secondBestFitted=population[i];
+function selection(){
+  for(var f=0; f<population.length; f++){
+    for(var j=0; j<population[f].fitness; j++){
+        matingPool.push(population[f]);
+    }
   }
 }
-
 function generateSon(){
+  mom=matingPool[Math.floor(Math.random()*matingPool.length)];
+  dad=matingPool[Math.floor(Math.random()*matingPool.length)];
+  matingPool.slice(0,matingPool.length);//svuoto l'array
   crossoverPoint=Math.round(Math.random()*7);
   offspring1= new Individual();
   offspring1.generateChromosome();
@@ -93,13 +98,13 @@ function generateSon(){
   offspring2.generateChromosome();
 
   //inizio crossover
-  for(var i=0; i<crossoverPoint; i++){
-      offspring1.chromosome[i]=bestFitted.chromosome[i];
-      offspring2.chromosome[i]=secondBestFitted.chromosome[i];
+  for(var g=0; g<crossoverPoint; g++){
+      offspring1.chromosome[g]=dad.chromosome[g];
+      offspring2.chromosome[g]=mom.chromosome[g];
     }
-    for(var i=crossoverPoint; i<bestFitted.chromosome.length; i++){
-        offspring2.chromosome[i]=bestFitted.chromosome[i];
-        offspring1.chromosome[i]=secondBestFitted.chromosome[i];
+    for(var h=crossoverPoint; h<bestFitted.chromosome.length; h++){
+        offspring2.chromosome[h]=dad.chromosome[h];
+        offspring1.chromosome[h]=mom.chromosome[h];
     }
     //mutazioni
     offspring1.mutation();
@@ -109,7 +114,6 @@ function generateSon(){
     //nascita
     population.push(offspring1);
     population.push(offspring2);
-
 }
 
 function killTheWeakest(){
